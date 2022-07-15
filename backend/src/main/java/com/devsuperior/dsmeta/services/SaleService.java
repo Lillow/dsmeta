602411551bd4b15afc91b962432de1d4208164a0
Danillo Silva <danillordm@gmail.com>
2022-07-15 16:16:48 -0300

    Fixs

--- backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -12,20 +12,19 @@ import org.springframework.stereotype.Service;
 import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.repositories.SaleRepository;
 
-
 @Service
 public class SaleService {
-	
+
 	@Autowired
 	private SaleRepository repository;
-	
-	public Page<Sale>findSales(String minDate, String maxDate, Pageable pageable) {
-		
+
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+
 		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
-		
+
 		LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
 		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
-		
+
 		return repository.findSales(min, max, pageable);
 	}
 
