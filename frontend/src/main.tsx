ac3e4162ea39926bb07736e57bc904b7abdbe4c0
Danillo Silva <danillordm@gmail.com>
2022-07-17 17:09:58 -0300

    Adjust notification

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+window.React = React
 import App from './App'
 import './index.css'
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
